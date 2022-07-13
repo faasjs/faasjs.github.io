b3d04f813dd1e80248886771b08c967f8e26144e
zfben <zfben@users.noreply.github.com>
2022-07-13 00:53:41 +0000

    deploy: faasjs/faasjs@4356b70a3a73de075d220b21a214ee5423624912

---
+++ assets/server.html.1236f519.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-0d55b212","path":"/zh/doc/server.html","title":"@faasjs/server","lang":"zh","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1657673579000},"filePathRelative":"zh/doc/server.md"}');export{e as data};
