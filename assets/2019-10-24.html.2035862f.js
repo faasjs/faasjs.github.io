1b3e98282fad2d5e941b0db675afc2444ab8d929
zfben <zfben@users.noreply.github.com>
2022-07-15 00:52:46 +0000

    deploy: faasjs/faasjs@db586948f1480db883b70252cd13227efe69d02e

---
+++ assets/2019-10-24.html.2035862f.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-08d35baa","path":"/zh/blog/2019-10-24.html","title":"FaasJS vs MVC","lang":"zh","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1657846329000},"filePathRelative":"zh/blog/2019-10-24.md"}');export{t as data};
---
+++ assets/aws.html.54ee5b13.js
@@ -0,0 +1,1 @@
+const a=JSON.parse('{"key":"v-32bfb01b","path":"/zh/doc/aws.html","title":"@faasjs/aws","lang":"zh","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1657846329000},"filePathRelative":"zh/doc/aws.md"}');export{a as data};
