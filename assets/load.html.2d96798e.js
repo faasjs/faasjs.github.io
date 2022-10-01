6db6b42ddf6270d8c1d5ac5a03acbc787c9921cf
zfben <zfben@users.noreply.github.com>
2022-10-01 03:09:06 +0000

    deploy: faasjs/faasjs@55fbab32147b6970ff8bf74772d46b2884058be8

--- assets/load.html.2d96798e.js
+++
@@ -1,1 +0,0 @@
-const a=JSON.parse('{"key":"v-3446e918","path":"/zh/doc/load.html","title":"@faasjs/load","lang":"zh","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1663293475000},"filePathRelative":"zh/doc/load.md"}');export{a as data};
--- assets/server.html.b64ee98a.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-0d55b212","path":"/zh/doc/server.html","title":"@faasjs/server","lang":"zh","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1663293475000},"filePathRelative":"zh/doc/server.md"}');export{e as data};
