450cfd166867e5281ce80dc251e61439855d4f23
zfben <zfben@users.noreply.github.com>
2022-10-28 04:17:57 +0000

    deploy: faasjs/faasjs@52a8edbad9a8f7b08875c11d6f9846152e7c9656

---
+++ assets/2021-07-10.html.d40daf36.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-eb2fb042","path":"/zh/blog/2021-07-10.html","title":"FaaS 开发之道","lang":"zh","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1666930637000},"filePathRelative":"zh/blog/2021-07-10.md"}');export{e as data};
