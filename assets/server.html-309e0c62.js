28fb390a6656bbae745426cc4851c8c744dcb466
zfben <zfben@users.noreply.github.com>
2023-04-05 07:27:48 +0000

    deploy: faasjs/faasjs@84c5861b22d57570723d3cfebcdbf99ed170adbe

---
+++ assets/server.html-309e0c62.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-0d55b212","path":"/zh/doc/server.html","title":"@faasjs/server","lang":"zh","frontmatter":{},"headers":[],"git":{"updatedTime":1680679626000},"filePathRelative":"zh/doc/server.md"}');export{e as data};
--- assets/tencentcloud.html-c888cc62.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-689c08ec","path":"/zh/doc/tencentcloud.html","title":"@faasjs/tencentcloud","lang":"zh","frontmatter":{},"headers":[],"git":{"updatedTime":1679576464000},"filePathRelative":"zh/doc/tencentcloud.md"}');export{t as data};
