e70ede8ad3bf12c8dd2a7537ecb84c565d9f8f8b
zfben <zfben@users.noreply.github.com>
2023-07-05 00:59:18 +0000

    deploy: faasjs/faasjs@f2e8bb8d15706644ebf1d565e3c70535f50ee609

--- assets/logger.html-83879f36.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,o as s,c as o,b as e,e as t}from"./app-46b838d7.js";const r={},c=e("h1",{id:"faasjs-logger",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#faasjs-logger","aria-hidden":"true"},"#"),t(" @faasjs/logger")],-1),n=e("p",null,"待补充",-1),_=[c,n];function l(d,i){return s(),o("div",null,_)}const h=a(r,[["render",l],["__file","logger.html.vue"]]);export{h as default};
