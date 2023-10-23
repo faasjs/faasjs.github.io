9310c32054c041382be1775e95a4d7f7fe0af607
zfben <zfben@users.noreply.github.com>
2023-10-23 23:59:43 +0000

    deploy: faasjs/faasjs@40be59990f357070bf91ec18d565b2ce8d45fbee

--- assets/load.html-22b02082.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,o as s,c as o,a as e,d as t}from"./app-437ca8bd.js";const c={},d=e("h1",{id:"faasjs-load",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#faasjs-load","aria-hidden":"true"},"#"),t(" @faasjs/load")],-1),n=e("p",null,"待补充",-1),r=[d,n];function _(l,i){return s(),o("div",null,r)}const h=a(c,[["render",_],["__file","load.html.vue"]]);export{h as default};
--- assets/server.html-e89a3f79.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-0d55b212","path":"/zh/doc/server.html","title":"@faasjs/server","lang":"zh","frontmatter":{},"headers":[],"git":{"updatedTime":1697804049000},"filePathRelative":"zh/doc/server.md"}');export{e as data};
