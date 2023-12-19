ee416b2c8c6f78097033f1819f01756432ced25e
zfben <zfben@users.noreply.github.com>
2023-12-19 01:40:16 +0000

    deploy: faasjs/faasjs@6aa260d0714ca7e6aef9fe746d241a311e756245

---
+++ assets/develop.html-SuK0uQhv.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-5c60213b","path":"/zh/doc/develop.html","title":"开发插件","lang":"zh","frontmatter":{},"headers":[{"level":2,"title":"插件的生命周期","slug":"插件的生命周期","link":"#插件的生命周期","children":[]}],"git":{"updatedTime":1702949976000},"filePathRelative":"zh/doc/develop.md"}');export{e as data};
--- assets/func.html-uHFBHWAR.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-ecf5af54","path":"/zh/doc/func.html","title":"@faasjs/func","lang":"zh","frontmatter":{},"headers":[{"level":2,"title":"使用方法","slug":"使用方法","link":"#使用方法","children":[]}],"git":{"updatedTime":1702256808000},"filePathRelative":"zh/doc/func.md"}');export{e as data};
