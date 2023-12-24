ff1ec2cc16e3db8b219e28a4ba5c838d5d767a5b
zfben <zfben@users.noreply.github.com>
2023-12-24 05:54:28 +0000

    deploy: faasjs/faasjs@f9a8335241ab6236fc8db588700d5fd1ea87ade4

---
+++ assets/index.html-9ehObaL6.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-81d78f3a","path":"/doc/jest/","title":"@faasjs/jest","lang":"en","frontmatter":{},"headers":[{"level":2,"title":"Install","slug":"install","link":"#install","children":[]},{"level":2,"title":"Usage","slug":"usage","link":"#usage","children":[]}],"git":{"updatedTime":null},"filePathRelative":"doc/jest/README.md"}');export{e as data};
