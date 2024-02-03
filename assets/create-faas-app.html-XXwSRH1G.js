3d165f560fd4698a777e3873782d8897188846a5
zfben <zfben@users.noreply.github.com>
2024-02-03 01:01:33 +0000

    deploy: faasjs/faasjs@9247c6d2bd0995b76a42eac1da733106840f3558

---
+++ assets/create-faas-app.html-XXwSRH1G.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,a}from"./app-ogmZakh0.js";const c={},r=a("h1",{id:"create-faas-app",tabindex:"-1"},[a("a",{class:"header-anchor",href:"#create-faas-app"},[a("span",null,"create-faas-app")])],-1),n=a("p",null,"待补充",-1),o=[r,n];function _(p,l){return t(),s("div",null,o)}const d=e(c,[["render",_],["__file","create-faas-app.html.vue"]]);export{d as default};
