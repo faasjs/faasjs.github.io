42f3ff01e2e57aabc67b146ecc78055da7ba8aa9
zfben <zfben@users.noreply.github.com>
2025-08-22 00:11:57 +0000

    deploy: faasjs/faasjs@2d4d814a36175fec439ffa9f4db804b4c4d938a8

---
+++ assets/func.html-Bg20LfwD.js
@@ -0,0 +1,17 @@
+import{_ as s,c as a,a as e,o as c}from"./app-DK9KTdMJ.js";const p={};function t(l,n){return c(),a("div",null,[...n[0]||(n[0]=[e(`<h1 id="faasjs-func" tabindex="-1"><a class="header-anchor" href="#faasjs-func"><span>@faasjs/func</span></a></h1><p>FaasJS 的主体模块，用于初始化云函数。</p><h2 id="使用方法" tabindex="-1"><a class="header-anchor" href="#使用方法"><span>使用方法</span></a></h2><div class="language-typescript line-numbers-mode" data-highlighter="prismjs" data-ext="ts"><pre><code class="language-typescript"><span class="line"><span class="token comment">// 引入 useFunc 方法</span></span>
+<span class="line"><span class="token keyword">import</span> <span class="token punctuation">{</span> useFunc <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">&#39;@faasjs/func&#39;</span></span>
+<span class="line"></span>
+<span class="line"><span class="token doc-comment comment">/**</span>
+<span class="line"> * 生成云函数主体，有以下注意点：</span>
+<span class="line"> * 1. 必须是 export const func = useFunc(function () <span class="token punctuation">{</span><span class="token punctuation">}</span>) 的形式</span>
+<span class="line"> * 2. 入参为一个函数，函数需要返回一个业务函数，且须为 Promise 函数</span>
+<span class="line"> */</span></span>
+<span class="line"><span class="token keyword">export</span> <span class="token keyword">const</span> func <span class="token operator">=</span> <span class="token function">useFunc</span><span class="token punctuation">(</span><span class="token keyword">function</span> <span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span></span>
+<span class="line">  <span class="token comment">// 若有需要初始化的插件，可以在这里初始化</span></span>
+<span class="line"></span>
+<span class="line">  <span class="token comment">// 返回业务函数</span></span>
+<span class="line">  <span class="token keyword">return</span> <span class="token keyword">async</span> <span class="token keyword">function</span> <span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span></span>
+<span class="line">    <span class="token comment">// 业务函数</span></span>
+<span class="line">  <span class="token punctuation">}</span></span>
+<span class="line"><span class="token punctuation">}</span><span class="token punctuation">)</span></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4)])])}const o=s(p,[["render",t]]),u=JSON.parse('{"path":"/zh/doc/func.html","title":"@faasjs/func","lang":"zh","frontmatter":{},"git":{"updatedTime":1755821487000,"contributors":[{"name":"Ben","username":"Ben","email":"ben@zfben.com","commits":1,"url":"https://github.com/Ben"}],"changelog":[{"hash":"2d4d814a36175fec439ffa9f4db804b4c4d938a8","time":1755821487000,"email":"ben@zfben.com","author":"Ben","message":"Merge pull request #2211 from faasjs/dependabot/github_actions/actions/checkout-5"}]},"filePathRelative":"zh/doc/func.md"}');export{o as comp,u as data};
